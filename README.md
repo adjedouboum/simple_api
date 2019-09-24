3b8ef15d0a089f0b172750be8efafa477b7e59f6
centos <centos@localhost.localdomain>
2019-09-24 17:22:20 -0400

    add deployment step

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # simple_api
+
 This repo is a simple example to build docker image from python API with azure devops pipeline to build it and push it on azure container registry
--- install_ansible.sh
+++ install_ansible.sh
@@ -2,4 +2,5 @@
 # script to install ansible to deploy some ressource using playbook  
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt -y update
-sudo apt -y install ansible
+sudo apt -y install ansible python-pip
+sudo pip install azure[azure] azure packaging msrestazure azure-storage 
